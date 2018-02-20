0f0cee50cae0a43002c40341120435c227aec1a3
Ryan Moore <moorer@udel.edu>
2018-02-20 18:34:44 -0500

    Remove old stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,14 @@ Rails.application.routes.draw do
   # resources :iroki_inputs
   # resources :iroki_outputs
   root 'pages#home'
-  post 'submit' => 'pages#submit', as: :submit
-  post 'jobs'   => 'pages#jobs'
 
-  # get 'snazzy_lala'   => 'pages#viewer',          as: :viewer # gives you viewer_path
-
-  get 'about'    => 'pages#about',           as: :about # gives you about_path
-  get 'contact'  => 'pages#contact',         as: :contact # gives you contact_path
-  get 'citation' => 'pages#citation',        as: :citation # gives you citation_path
-  get 'download' => 'pages#download_result', as: :download
-  get 'error' => 'pages#error', as: :error
-  # get 'jobs'     => 'pages#jobs',     as: :jobs
-  #
+  get 'about' => 'pages#about', as: :about # gives you about_path
+  get 'contact' => 'pages#contact', as: :contact # gives you contact_path
+  get 'citation' => 'pages#citation', as: :citation # gives you citation_path
 
   get 'docs' => 'pages#docs', as: :docs
 
   get 'biom' => 'pages#biom', as: :biom
-
-  # get 'viewer' => 'pages#home', as: :viewer
-  # get 'viewer', to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
