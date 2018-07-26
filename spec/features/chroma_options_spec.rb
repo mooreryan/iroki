fb868b66c77d5c4744a0cc2006f106ee80ea74ee
Ryan Moore <moorer@udel.edu>
2018-07-26 19:11:30 -0400

    Fix test

--- spec/features/chroma_options_spec.rb
+++ spec/features/chroma_options_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe "layouts/biom/_chroma_options.html.slim", js: true do
   it "corrects bad values for chroma min" do
     visit biom_path
 
+    # See https://stackoverflow.com/questions/20134085/how-to-select-option-in-drop-down-using-capybara
+    find("select#biom-conversion-style").find("option#biom-conversion-style-geometry").select_option
+
+    click_link "Options"
+
     click_link "Chroma (saturation) options"
 
     fill_in("chroma-min", with: "-1234")
