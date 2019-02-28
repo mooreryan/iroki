c418aa42a8362472195191671671ec17abf6abde
Ryan Moore <moorer@udel.edu>
2019-02-27 22:18:24 -0500

    Refactor

--- spec/javascripts/viewer_spec.js
+++ spec/javascripts/viewer_spec.js
@@ -14,47 +14,49 @@ describe("viewer", function () {
           reset_all_to_defaults();
         });
 
-        it("sets default layout shape", function () {
-          expect(
-            $("#" + global.html.id.tree_layout).val()
-          ).to.equal(global.html.id.tree_layout_radial);
+        describe("tree format options", function () {
+          it("sets default layout shape", function () {
+            expect(
+              $("#" + global.html.id.tree_layout).val()
+            ).to.equal(global.html.id.tree_layout_radial);
 
-          expect(
-            $("#" + global.html.id.tree_layout_radial).prop("selected")
-          ).to.be.true;
+            expect(
+              $("#" + global.html.id.tree_layout_radial).prop("selected")
+            ).to.be.true;
+          });
+
+          it("sets default branch style", function () {
+            expect(
+              $("#" + global.html.id.tree_branch_style).val()
+            ).to.equal(global.html.id.tree_branch_style_normal);
+
+            expect(
+              $("#" + global.html.id.tree_branch_style_normal).prop("selected")
+            ).to.be.true;
+          });
+
+          it("sets default sorting", function () {
+            expect(
+              $("#" + global.html.id.tree_sorting).val()
+            ).to.equal(global.html.id.tree_sorting_forward);
+
+            expect(
+              $("#" + global.html.id.tree_sorting_forward).prop("selected")
+            ).to.be.true;
+          });
+
+          it("sets default tree rotation", function () {
+            expect(
+              parseInt($("#" + global.html.id.tree_rotation).val())
+            ).to.equal(viewer.defaults.tree_rotation);
+          });
+
+          it("sets default tree root option", function () {
+            expect(
+              $("#" + global.html.id.biologically_rooted).prop("checked")
+            ).to.be.true;
+          });
         });
-
-        it("sets default branch style", function () {
-          expect(
-            $("#" + global.html.id.tree_branch_style).val()
-          ).to.equal(global.html.id.tree_branch_style_normal);
-
-          expect(
-            $("#" + global.html.id.tree_branch_style_normal).prop("selected")
-          ).to.be.true;
-        });
-
-        it("sets default sorting", function () {
-          expect(
-            $("#" + global.html.id.tree_sorting).val()
-          ).to.equal(global.html.id.tree_sorting_forward);
-
-          expect(
-            $("#" + global.html.id.tree_sorting_forward).prop("selected")
-          ).to.be.true;
-        });
-
-        it("sets default tree rotation", function () {
-          expect(
-            parseInt($("#" + global.html.id.tree_rotation).val())
-          ).to.equal(viewer.defaults.tree_rotation);
-        });
-
-        it("sets default tree root option", function() {
-          expect(
-            $("#" + global.html.id.biologically_rooted).prop("checked")
-          ).to.be.true;
-        })
       });
     });
   });
