3d3cdcab47dfc3ab0b3c6ad6470995f2fee6ce59
Ryan Moore <moorer@udel.edu>
2020-06-29 16:09:33 -0400

    Fix controller specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
-  end
+routes = [:about,
+          :biom,
+          :canvas_viewer,
+          :classify,
+          :contact,
+          :citation,
+          :pd,
+          :tree_cluster,
+          :viewer]
 
+RSpec.describe PagesController, type: :controller do
   # @note Everything that should render :error here is handled by controller logic
 
-
-  describe "GET #contact" do
-    it "returns http success" do
-      get :contact
-      expect(response).to have_http_status(:success)
+  shared_examples_for "returns http success" do |route|
+    describe "GET ##{route}" do
+      it "returns http success" do
+        get route.to_sym
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  describe "GET #citation" do
-    it "returns http success" do
-      get :citation
-      expect(response).to have_http_status(:success)
-    end
+  routes.each do |route|
+    include_examples "returns http success", route
   end
-
 end
